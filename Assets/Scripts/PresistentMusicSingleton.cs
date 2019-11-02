94b7f7eae395f2dffb9679c48f00c6ce590b551d
Gergely Óvári-Timár <>
2019-11-02 19:09:07 +0100

    Restliche 2 Prefabs vom letztem Commit.

---
+++ Assets/Scripts/PresistentMusicSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresistentMusicSingleton : MonoBehaviour
+{
+
+	static PresistentMusicSingleton instance;
+
+	public static PresistentMusicSingleton Instance {
+		get {
+			return instance;
+		}
+
+		set {
+			instance = value;
+		}
+	}
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		if(instance == null){
+			instance = this;
+		}else if (instance != this){
+			Destroy(gameObject);
+		}
+
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
