41c22b65f4a275d05cf4276bfa7e238747a53614
lateKnight <mbsulz@yahoo.de>
2019-11-24 17:35:06 +0100

    Update: IntroText.cs now inherits from UIPanel.cs

--- Assets/Scripts/UI/MainMenu/IntroText.cs
+++ Assets/Scripts/UI/MainMenu/IntroText.cs
@@ -1,28 +1,17 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class IntroText : MonoBehaviour
+public class IntroText : UIPanel
 {
-    private Animator animator;
     [SerializeField] private Animator menuAnimator;
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.anyKey)
         {
-            HideText();
+            Hide();
         }
-    }
-
-    void HideText()
-    {
-        animator.SetBool("show", false);
     }
 
     void ShowMenu()
