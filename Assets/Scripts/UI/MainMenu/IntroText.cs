5cd31b68c0ea301d74e8147aa47286091e6d67e3
lateKnight <mbsulz@yahoo.de>
2019-11-24 15:11:43 +0100

    Add: script to toggle menu buttons, hides idle text

---
+++ Assets/Scripts/UI/MainMenu/IntroText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IntroText : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private Animator menuAnimator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            HideText();
+        }
+    }
+
+    void HideText()
+    {
+        animator.SetBool("show", false);
+    }
+
+    void ShowMenu()
+    {
+        menuAnimator.SetBool("show", true);
+    }
+}
